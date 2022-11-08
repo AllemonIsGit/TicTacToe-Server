7273b676b8df278ab8f612f8f64aecdaaf3386df
Allemon <allemon3@gmail.com>
2022-11-08 17:00:21 +0100

    Added TokenUtil

--- src/main/java/com/example/TicTacToe/token/JWTTokenUtil.java
+++ src/main/java/com/example/TicTacToe/token/JWTTokenUtil.java
@@ -3,10 +3,28 @@ package com.example.TicTacToe.token;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Component
 public class JWTTokenUtil {
     private final Algorithm algorithm = Algorithm.HMAC512(System.getenv("ALGORITHM_KEY"));
     private final JWTVerifier verifier = JWT.require(algorithm).build();
+
+    public String createToken(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuedAt(Date.from(Instant.now()))
+                .withIssuer("TicTacToe_Server")
+                .withExpiresAt(Date.from(Instant.now().plus(15, ChronoUnit.MINUTES)))
+                .sign(algorithm);
+    }
+
+    public DecodedJWT validateToken(String token) {
+        return verifier.verify(token);
+    }
 }
